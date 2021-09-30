4269644aed9a8bba70903b48a1305e9a17e4ec1a
CaseyLee3 <>
2021-09-30 18:52:27 +0000

    Main file, first submission

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+
+  cout << "Hello darkness my old friend" << endl;
+
+  cout << "Ive come to you again" << endl;
+  
+}

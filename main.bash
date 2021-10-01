1d02400c026eee8d87e31b80f3267aefa9135cc9
CaseyLee3 <>
2021-10-01 12:06:08 +0000

    changes

---
+++ main.bash
@@ -0,0 +1,2 @@
+clang++ -Wall -std=c++11 main.cpp -o test.exe
+./test.exe
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+
+  cout << "Hello darkness my old friend" << endl;
+
+  cout << "Ive come to you again" << endl;
+  
+}
